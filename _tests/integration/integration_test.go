e7349e500e26fa76754b609501336ee88e1da8f9
trapacska <trapacska@gmail.com>
2017-11-29 11:29:53 +0100

    added no-audio flag

--- _tests/integration/integration_test.go
+++ _tests/integration/integration_test.go
@@ -156,7 +156,7 @@ func startEmulator(t *testing.T, runOptions []string) {
 	emulator, err := emulatormanager.New(androidSdk)
 	require.NoError(t, err)
 
-	options := []string{"-no-boot-anim", "-no-window", "-gpu", "off"}
+	options := []string{"-no-boot-anim", "-no-window", "-gpu", "off", "-no-audio"}
 	options = append(options, runOptions...)
 
 	startEmulatorCommand := emulator.StartEmulatorCommand(testEmulatorName, testEmulatorSkin, options...)
