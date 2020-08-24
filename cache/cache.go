e946d4e3aa64bb0c7a2e6c6850fe02080b3de3a8
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2020-08-24 07:31:23 +0200

    Exclude old Gradle daemon, wrapper and caches directoryies from build cache.

--- cache/cache.go
+++ cache/cache.go
@@ -4,39 +4,147 @@ import (
 	"crypto/md5"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strings"
 
 	"github.com/bitrise-io/go-steputils/cache"
+	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/errorutil"
 	"github.com/bitrise-io/go-utils/fileutil"
 	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/hashicorp/go-version"
 )
 
 // Level defines the extent to which caching should be used.
 // - LevelNone: no caching
 // - LevelDeps: only dependencies will be cached
 // - LevelAll: caching will include gradle and android build cache
-type Level int
+type Level string
 
 // Cache level
 const (
-	LevelNone Level = iota
-	LevelDeps
-	LevelAll
+	LevelNone = Level("none")
+	LevelDeps = Level("only_deps")
+	LevelAll  = Level("all")
 )
 
-// ParseLevel ...
-func ParseLevel(s string) Level {
-	switch s {
-	case "all":
-		return LevelAll
-	case "only_deps":
-		return LevelDeps
-	default:
-		return LevelNone
+func parseGradlewVersion(out string) (string, error) {
+	/*
+	   ------------------------------------------------------------
+	   Gradle 6.1.1
+	   ------------------------------------------------------------
+
+	   Build time:   2020-01-24 22:30:24 UTC
+	   Revision:     a8c3750babb99d1894378073499d6716a1a1fa5d
+
+	   Kotlin:       1.3.61
+	   Groovy:       2.5.8
+	   Ant:          Apache Ant(TM) version 1.10.7 compiled on September 1 2019
+	   JVM:          1.8.0_241 (Oracle Corporation 25.241-b07)
+	   OS:           Mac OS X 10.15.5 x86_64
+	*/
+
+	pattern := `Gradle (.*)`
+	exp := regexp.MustCompile(pattern)
+	matches := exp.FindStringSubmatch(out)
+	if len(matches) < 2 {
+		return "", fmt.Errorf("failed to find Gradle version in output:\n%s\nusing a pattern:%s", out, pattern)
 	}
+	return matches[1], nil
+}
+
+func projectGradleVersion(projectPth string) (string, error) {
+	gradlewPth := filepath.Join(projectPth, "gradlew")
+	exist, err := pathutil.IsPathExists(gradlewPth)
+	if err != nil {
+		return "", fmt.Errorf("failed to check if %s exists: %s", gradlewPth, err)
+	}
+	if !exist {
+		return "", fmt.Errorf("no gradlew found at: %s", gradlewPth)
+	}
+
+	versionCmd := command.New("./gradlew", "-version")
+	versionCmd.SetDir(filepath.Dir(gradlewPth))
+	out, err := versionCmd.RunAndReturnTrimmedCombinedOutput()
+	if err != nil {
+		if errorutil.IsExitStatusError(err) {
+			return "", fmt.Errorf("%s failed: %s", versionCmd.PrintableCommandArgs(), out)
+		}
+		return "", fmt.Errorf("%s failed: %s", versionCmd.PrintableCommandArgs(), err)
+	}
+
+	return parseGradlewVersion(out)
+}
+
+func oldGradleExcludePaths(homeDir, currentGradleVersion string) ([]string, error) {
+	var excludes []string
+
+	gradlewDir := filepath.Join(homeDir, ".gradle")
+	exist, err := pathutil.IsPathExists(gradlewDir)
+	if err != nil {
+		return nil, fmt.Errorf("failed to check if %s exist: %s", gradlewDir, err)
+	}
+	if !exist {
+		return nil, nil
+	}
+
+	{
+		// exclude old wrappers, like ~/.gradle/wrapper/dists/gradle-5.1.1-all
+		wrapperDistrDir := filepath.Join(gradlewDir, "wrapper", "dists")
+		entries, err := ioutil.ReadDir(wrapperDistrDir)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read entries of %s: %s", wrapperDistrDir, err)
+		}
+		for _, e := range entries {
+			if !strings.HasPrefix(e.Name(), "gradle-"+currentGradleVersion) {
+				excludes = append(excludes, "!"+filepath.Join(wrapperDistrDir, e.Name()))
+			}
+		}
+	}
+
+	{
+		// exclude old caches, like ~/.gradle/caches/5.1.1
+		cachesDir := filepath.Join(gradlewDir, "caches")
+		entries, err := ioutil.ReadDir(cachesDir)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read entries of %s: %s", cachesDir, err)
+		}
+		for _, e := range entries {
+			v, err := version.NewVersion(e.Name())
+			if err != nil || v == nil {
+				continue
+			}
+
+			if e.Name() != currentGradleVersion {
+				excludes = append(excludes, "!"+filepath.Join(cachesDir, e.Name()))
+			}
+		}
+	}
+
+	{
+		// exclude old daemon, like ~/.gradle/daemon/5.1.1
+		daemonDir := filepath.Join(gradlewDir, "daemon")
+		entries, err := ioutil.ReadDir(daemonDir)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read entries of %s: %s", daemonDir, err)
+		}
+		for _, e := range entries {
+			v, err := version.NewVersion(e.Name())
+			if err != nil || v == nil {
+				continue
+			}
+
+			if e.Name() != currentGradleVersion {
+				excludes = append(excludes, "!"+filepath.Join(daemonDir, e.Name()))
+			}
+		}
+	}
+
+	return excludes, nil
 }
 
 // Collect walks the directory tree underneath projectRoot and registers matching
@@ -49,15 +157,8 @@ func Collect(projectRoot string, cacheLevel Level) error {
 	}
 
 	gradleCache := cache.New()
-	homeDir := pathutil.UserHomeDir()
 
-	projectRoot, err := filepath.Abs(projectRoot)
-	if err != nil {
-		return fmt.Errorf("cache collection skipped: failed to determine project root path")
-	}
-
-	lockFilePath := filepath.Join(projectRoot, "gradle.deps")
-
+	var includePths []string
 	excludePths := []string{
 		"~/.gradle/**",
 		"!~/.gradle/daemon/*/daemon-*.out.log", // excludes Gradle daemon logs, like: ~/.gradle/daemon/6.1.1/daemon-3122.out.log
@@ -76,32 +177,50 @@ func Collect(projectRoot string, cacheLevel Level) error {
 		"!*.apk",
 	}
 
-	var includePths []string
+	homeDir := pathutil.UserHomeDir()
 
-	if cacheLevel >= LevelDeps {
-		lockfileContent := ""
-		if err := filepath.Walk(projectRoot, func(path string, f os.FileInfo, err error) error {
-			if !f.IsDir() && strings.HasSuffix(f.Name(), ".gradle") && !strings.Contains(path, "node_modules") {
-				if md5Hash, err := computeMD5String(path); err != nil {
-					log.Warnf("Failed to compute MD5 hash of file(%s), error: %s", path, err)
-				} else {
-					lockfileContent += md5Hash
-				}
-			}
-			return nil
-		}); err != nil {
-			return fmt.Errorf("dependency map generation skipped: failed to collect dependencies")
-		}
-		if err := fileutil.WriteStringToFile(lockFilePath, lockfileContent); err != nil {
-			return fmt.Errorf("dependency map generation skipped: failed to write lockfile, error: %s", err)
-		}
-
-		includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".gradle"), lockFilePath))
-		includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".kotlin"), lockFilePath))
-		includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".m2"), lockFilePath))
+	projectRoot, err := filepath.Abs(projectRoot)
+	if err != nil {
+		return fmt.Errorf("cache collection skipped: failed to determine project root path")
 	}
 
-	if cacheLevel >= LevelAll {
+	ver, err := projectGradleVersion(projectRoot)
+	if err != nil {
+		log.Warnf("failed to get project gradle version: %s", err)
+	} else {
+		excludes, err := oldGradleExcludePaths(homeDir, ver)
+		if err != nil {
+			log.Warnf("failed to collect old gradle exclude paths: %s", err)
+		} else {
+			fmt.Printf("old gradle exclude path:\n%s\n", strings.Join(excludes, "\n"))
+			excludePths = append(excludePths, excludes...)
+		}
+	}
+
+	lockFilePath := filepath.Join(projectRoot, "gradle.deps")
+
+	lockfileContent := ""
+	if err := filepath.Walk(projectRoot, func(path string, f os.FileInfo, err error) error {
+		if !f.IsDir() && strings.HasSuffix(f.Name(), ".gradle") && !strings.Contains(path, "node_modules") {
+			if md5Hash, err := computeMD5String(path); err != nil {
+				log.Warnf("Failed to compute MD5 hash of file(%s), error: %s", path, err)
+			} else {
+				lockfileContent += md5Hash
+			}
+		}
+		return nil
+	}); err != nil {
+		return fmt.Errorf("dependency map generation skipped: failed to collect dependencies")
+	}
+	if err := fileutil.WriteStringToFile(lockFilePath, lockfileContent); err != nil {
+		return fmt.Errorf("dependency map generation skipped: failed to write lockfile, error: %s", err)
+	}
+
+	includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".gradle"), lockFilePath))
+	includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".kotlin"), lockFilePath))
+	includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".m2"), lockFilePath))
+
+	if cacheLevel == LevelAll {
 		includePths = append(includePths, fmt.Sprintf("%s -> %s", filepath.Join(homeDir, ".android", "build-cache"), lockFilePath))
 
 		if err := filepath.Walk(projectRoot, func(path string, f os.FileInfo, err error) error {
---
+++ cache/cache_test.go
@@ -0,0 +1,43 @@
+package cache
+
+import "testing"
+
+func Test_parseGradlewVersion(t *testing.T) {
+	tests := []struct {
+		name    string
+		out     string
+		want    string
+		wantErr bool
+	}{
+		{
+			name: "",
+			out: `
+------------------------------------------------------------
+Gradle 6.1.1
+------------------------------------------------------------
+
+Build time:   2020-01-24 22:30:24 UTC
+Revision:     a8c3750babb99d1894378073499d6716a1a1fa5d
+
+Kotlin:       1.3.61
+Groovy:       2.5.8
+Ant:          Apache Ant(TM) version 1.10.7 compiled on September 1 2019
+JVM:          1.8.0_241 (Oracle Corporation 25.241-b07)
+OS:           Mac OS X 10.15.5 x86_64`,
+			want:    "6.1.1",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := parseGradlewVersion(tt.out)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("parseGradlewVersion() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if got != tt.want {
+				t.Errorf("parseGradlewVersion() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
