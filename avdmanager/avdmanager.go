493bc932df3daa6b654d847c4c3fb0f8e658baa9
Laszlo Pusok <laszlo.pusok@bitrise.io>
2021-05-27 14:02:02 +0200

    Restored previous fallback logic

--- avdmanager/avdmanager.go
+++ avdmanager/avdmanager.go
@@ -2,12 +2,16 @@ package avdmanager
 
 import (
 	"fmt"
+	"os"
 	"path/filepath"
 
 	"github.com/bitrise-io/go-android/sdk"
 	"github.com/bitrise-io/go-android/sdkcomponent"
+	"github.com/bitrise-io/go-android/sdkmanager"
 	"github.com/bitrise-io/go-utils/command"
+	"github.com/bitrise-io/go-utils/log"
 	"github.com/bitrise-io/go-utils/pathutil"
+	"github.com/bitrise-tools/go-android/sdkmanager"
 )
 
 // Model ...
@@ -30,6 +34,30 @@ func New(sdk sdk.AndroidSdkInterface) (*Model, error) {
 		return &Model{
 			binPth: avdmanagerPath,
 		}, nil
+	}
+
+	if SDKManager, err := sdkmanager.New(sdk); err != nil {
+		log.Warnf("%v", err)
+	} else if !SDKManager.IsLegacySDK() {
+		fmt.Println()
+		log.Warnf("Found sdkmanager but no avdmanager, updating SDK Tools...")
+
+		sdkToolComponent := sdkcomponent.SDKTool{}
+		updateCmd := SDKManager.InstallCommand(sdkToolComponent)
+		updateCmd.SetStderr(os.Stderr).SetStdout(os.Stdout)
+		log.Infof("$ %s", updateCmd.PrintableCommandArgs())
+		if err := updateCmd.Run(); err != nil {
+			log.Errorf("Installing avdmanager failed: %v", err)
+		} else {
+			if exists, err := pathutil.IsPathExists(avdmanagerPath); err != nil {
+				return nil, err
+			} else if exists {
+				return &Model{
+					binPth: avdmanagerPath,
+				}, nil
+			}
+		}
+		log.Printf("Updating SDK tools was unsuccessful, continuing with legacy avdmanager...")
 	}
 
 	legacyAvdmanagerPath := filepath.Join(sdk.GetAndroidHome(), "tools", "android")
