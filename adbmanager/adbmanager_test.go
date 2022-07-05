eb6c4f958d711bc12a9da3ba34e82dcfa38d3b77
Olivér Falvai <oliver.falvai@bitrise.io>
2022-07-05 15:45:21 +0200

    Add new ADB commands (#53)
    * Add new ADB commands
    
    * Add kill-server command
    
    * Add command.Opts argument
    
    * Add test

--- adbmanager/adbmanager_test.go
+++ adbmanager/adbmanager_test.go
@@ -63,6 +63,24 @@ func Test_GivenAdditionalTestingOptions_WhenCreateRunInstrumentedTestsCmd_ThenCr
 	require.Equal(t, expectedArgs, actualArgs)
 }
 
+func Test_GivenShellCommand_WhenCreateWaitForDeviceCmd_ThenCreatesExpectedCommand(t *testing.T) {
+	// Given
+	serial := "emulator-5554"
+	shellCmd := "getprop sys.boot_completed"
+
+	// When
+	testCommand := mockModel().WaitForDeviceThenShellCmd(
+		serial,
+		&command.Opts{},
+		shellCmd,
+	)
+
+	// Then
+	actualArgs := testCommand.PrintableCommandArgs()
+	expectedArgs := `adb "-s" "emulator-5554" "wait-for-device" "shell" "getprop sys.boot_completed"`
+	require.Equal(t, expectedArgs, actualArgs)
+}
+
 // Helpers
 
 func mockModel() Model {
