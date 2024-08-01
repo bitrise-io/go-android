a416a7cd79e5115b9c5533d4e64b664f35920328
Gabor Pongracz <gabor.pongracz@bitrise.io>
2024-08-01 17:02:11 +0200

    Add json tags

--- metaparser/androidartifact/artifact.go
+++ metaparser/androidartifact/artifact.go
@@ -1,10 +1,10 @@
 package androidartifact
 
 type Info struct {
-	AppName           string
-	PackageName       string
-	VersionCode       string
-	VersionName       string
-	MinSDKVersion     string
-	RawPackageContent string
+	AppName           string `json:"app_name"`
+	PackageName       string `json:"package_name"`
+	VersionCode       string `json:"version_code"`
+	VersionName       string `json:"version_name"`
+	MinSDKVersion     string `json:"min_sdk_version"`
+	RawPackageContent string `json:"-"`
 }
--- metaparser/meta.go
+++ metaparser/meta.go
@@ -3,12 +3,12 @@ package parser
 import "github.com/bitrise-io/go-android/v2/metaparser/androidartifact"
 
 type ArtifactMetadata struct {
-	AppInfo        androidartifact.Info
-	FileSizeBytes  int64
-	Module         string
-	ProductFlavour string
-	BuildType      string
-	SignedBy       string
-	Warnings       []string
+	AppInfo        androidartifact.Info `json:"app_info"`
+	FileSizeBytes  int64                `json:"file_size_bytes"`
+	Module         string               `json:"module"`
+	ProductFlavour string               `json:"product_flavour"`
+	BuildType      string               `json:"build_type"`
+	SignedBy       string               `json:"signed_by"`
+	Warnings       []string             `json:"warnings"`
 	androidartifact.Artifact
 }
