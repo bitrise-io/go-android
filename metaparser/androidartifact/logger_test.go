930686a4f51c5fb624db93b97bf266ffc6554119
Gabor Pongracz <gabor.pongracz@bitrise.io>
2024-08-06 12:55:54 +0200

    Fix test logger

--- metaparser/androidartifact/logger_test.go
+++ metaparser/androidartifact/logger_test.go
@@ -11,3 +11,17 @@ func (l *testLogger) AABParseWarnf(_, format string, args ...interface{}) {
 func (l *testLogger) APKParseWarnf(_, format string, args ...interface{}) {
 	fmt.Printf(format, args...)
 }
+
+func (l *testLogger) Infof(format string, args ...interface{})   { fmt.Printf(format, args...) }
+func (l *testLogger) Printf(format string, args ...interface{})  { fmt.Printf(format, args...) }
+func (l *testLogger) Donef(format string, args ...interface{})   { fmt.Printf(format, args...) }
+func (l *testLogger) Debugf(format string, args ...interface{})  { fmt.Printf(format, args...) }
+func (l *testLogger) Errorf(format string, args ...interface{})  { fmt.Printf(format, args...) }
+func (l *testLogger) TInfof(format string, args ...interface{})  { fmt.Printf(format, args...) }
+func (l *testLogger) TWarnf(format string, args ...interface{})  { fmt.Printf(format, args...) }
+func (l *testLogger) TPrintf(format string, args ...interface{}) { fmt.Printf(format, args...) }
+func (l *testLogger) TDonef(format string, args ...interface{})  { fmt.Printf(format, args...) }
+func (l *testLogger) TDebugf(format string, args ...interface{}) { fmt.Printf(format, args...) }
+func (l *testLogger) TErrorf(format string, args ...interface{}) { fmt.Printf(format, args...) }
+func (l *testLogger) Println()                                   { fmt.Println() }
+func (l *testLogger) EnableDebugLog(_ bool)                      {}
