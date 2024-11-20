8b681d88ae87adf71e1bf4f7f9256777ed33e235
Krisztián Gödrei <krisztian.godrei@bitrise.io>
2024-11-20 10:33:44 +0100

    Remove vendor dir

--- vendor/github.com/avast/apkparser/go.mod
+++
@@ -1,8 +0,0 @@
-module github.com/avast/apkparser
-
-go 1.17
-
-require (
-	github.com/avast/apkverifier v0.0.0-20231031113634-2a81e2edb41e
-	github.com/klauspost/compress v1.16.6
-)
--- vendor/github.com/avast/apkparser/runtests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eux
-
-if [ -n "${GIMME_OS:-}" ] && [ "$GIMME_OS" != "linux" ]; then
-    echo "Non-linux travis platform, skipping tests..."
-    exit 0
-fi
-
-go test -v ./...
