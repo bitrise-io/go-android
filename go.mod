646e446760c6975beefc0b68d485223bb0b138c6
LaszloP <7979773+lpusok@users.noreply.github.com>
2025-08-25 15:59:40 +0200

    Fix unicode in apk app name

--- go.mod
+++ go.mod
@@ -1,18 +1,28 @@
 module github.com/bitrise-io/go-android/v2
 
-go 1.16
+go 1.23.0
 
 require (
-	github.com/avast/apkparser v0.0.0-20240729092610-90591e0804ae
+	github.com/avast/apkparser v0.0.0-20250626104540-d53391f4d69d
 	github.com/bitrise-io/go-pkcs12 v0.1.0
 	github.com/bitrise-io/go-steputils v1.0.6
-	github.com/bitrise-io/go-utils v1.0.13
-	github.com/bitrise-io/go-utils/v2 v2.0.0-alpha.23
+	github.com/bitrise-io/go-utils v1.0.15
+	github.com/bitrise-io/go-utils/v2 v2.0.0-alpha.25
 	github.com/hashicorp/go-version v1.7.0
-	github.com/klauspost/compress v1.17.9 // indirect
 	github.com/kr/pretty v0.3.1
-	github.com/lwithers/minijks v1.1.0
+	github.com/lwithers/minijks v1.2.0
 	github.com/ryanuber/go-glob v1.0.0
 	github.com/stretchr/testify v1.9.0
-	golang.org/x/crypto v0.25.0 // indirect
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.8 // indirect
+	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	golang.org/x/crypto v0.41.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- metaparser/androidartifact/apk_info_test.go
+++ metaparser/androidartifact/apk_info_test.go
@@ -3,14 +3,26 @@ package androidartifact
 import (
 	"os"
 	"path"
-	"reflect"
 	"testing"
 
 	"github.com/bitrise-io/go-utils/command/git"
 	"github.com/bitrise-io/go-utils/log"
 )
 
-func Test_GetAPKInfoWithFallback(t *testing.T) {
+const testArtifactAndroidManifest string = `<manifest xmlns:android="http://schemas.android.com/apk/res/android" android:versionCode="1" android:versionName="1.0" package="com.example.birmachera.myapplication">
+	<uses-sdk android:minSdkVersion="17" android:targetSdkVersion="28"></uses-sdk>
+	<uses-permission android:name="android.permission.INTERNET"></uses-permission>
+	<application android:theme="null" android:label="My Application" android:icon="res/mipmap-xxxhdpi-v4/ic_launcher.png" android:debuggable="true" android:allowBackup="true" android:supportsRtl="true" android:roundIcon="res/mipmap-xxxhdpi-v4/ic_launcher_round.png" android:appComponentFactory="android.support.v4.app.CoreComponentFactory">
+		<activity android:name="com.example.birmachera.myapplication.MainActivity">
+			<intent-filter>
+				<action android:name="android.intent.action.MAIN"></action>
+				<category android:name="android.intent.category.LAUNCHER"></category>
+			</intent-filter>
+		</activity>
+	</application>
+</manifest>`
+
+func TestGetAPKInfoWithFallback(t *testing.T) {
 	tLogger := &testLogger{}
 	tmpDir, err := os.MkdirTemp("", "")
 	if err != nil {
@@ -31,34 +43,68 @@ func Test_GetAPKInfoWithFallback(t *testing.T) {
 		t.Fatalf("setup: failed to clone test artifact repo, error: %s", err)
 	}
 
-	apkPath := path.Join(tmpDir, "apks", "app-debug.apk")
-	got, err := GetAPKInfoWithFallback(tLogger, apkPath)
-	if err != nil {
-		t.Fatalf("GetAPKInfo() error = %v", err)
+	type args struct {
+		logger Logger
+		apkPth string
 	}
+	tests := []struct {
+		name    string
+		apkPath string
+		want    Info
+		wantErr bool
+	}{
+		{
+			name:    "valid apk",
+			apkPath: path.Join(tmpDir, "apks", "app-debug.apk"),
+			want: Info{
+				AppName:           "My Application",
+				PackageName:       "com.example.birmachera.myapplication",
+				VersionCode:       "1",
+				VersionName:       "1.0",
+				MinSDKVersion:     "17",
+				RawPackageContent: testArtifactAndroidManifest,
+			},
+			wantErr: false,
+		},
+		{
+			name:    "apk with unicode app name",
+			apkPath: path.Join(tmpDir, "apks", "app-debug-uncode-name.apk"),
+			want: Info{
+				AppName:       "My Application Unicode 🤪",
+				PackageName:   "com.example.myapplicationUnicode",
+				VersionCode:   "1",
+				VersionName:   "1.0",
+				MinSDKVersion: "33",
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GetAPKInfoWithFallback(tLogger, tt.apkPath)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetAPKInfoWithFallback() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 
-	want := Info{
-		AppName:           "My Application",
-		PackageName:       "com.example.birmachera.myapplication",
-		VersionCode:       "1",
-		VersionName:       "1.0",
-		MinSDKVersion:     "17",
-		RawPackageContent: testArtifactAndroidManifest,
-	}
-	if !reflect.DeepEqual(got, want) {
-		t.Fatalf("GetAPKInfo() = %+v, want %+v", got, want)
+			if got.AppName != tt.want.AppName {
+				t.Fatalf("GetAPKInfo() app name = %+v, want %+v", got.AppName, tt.want.AppName)
+			}
+			if got.PackageName != tt.want.PackageName {
+				t.Fatalf("GetAPKInfo() package name = %+v, want %+v", got.PackageName, tt.want.PackageName)
+			}
+			if got.VersionCode != tt.want.VersionCode {
+				t.Fatalf("GetAPKInfo() version code = %+v, want %+v", got.VersionCode, tt.want.VersionCode)
+			}
+			if got.VersionName != tt.want.VersionName {
+				t.Fatalf("GetAPKInfo() version name = %+v, want %+v", got.VersionName, tt.want.VersionName)
+			}
+			if got.MinSDKVersion != tt.want.MinSDKVersion {
+				t.Fatalf("GetAPKInfo() min sdk version = %+v, want %+v", got.MinSDKVersion, tt.want.MinSDKVersion)
+			}
+			if tt.want.RawPackageContent != "" && got.RawPackageContent != tt.want.RawPackageContent {
+				t.Fatalf("GetAPKInfo() raw package content = %+v, want %+v", got.RawPackageContent, tt.want.RawPackageContent)
+			}
+		})
 	}
 }
-
-const testArtifactAndroidManifest string = `<manifest xmlns:android="http://schemas.android.com/apk/res/android" android:versionCode="1" android:versionName="1.0" package="com.example.birmachera.myapplication">
-	<uses-sdk android:minSdkVersion="17" android:targetSdkVersion="28"></uses-sdk>
-	<uses-permission android:name="android.permission.INTERNET"></uses-permission>
-	<application android:theme="null" android:label="My Application" android:icon="res/mipmap-xxxhdpi-v4/ic_launcher.png" android:debuggable="true" android:allowBackup="true" android:supportsRtl="true" android:roundIcon="res/mipmap-xxxhdpi-v4/ic_launcher_round.png" android:appComponentFactory="android.support.v4.app.CoreComponentFactory">
-		<activity android:name="com.example.birmachera.myapplication.MainActivity">
-			<intent-filter>
-				<action android:name="android.intent.action.MAIN"></action>
-				<category android:name="android.intent.category.LAUNCHER"></category>
-			</intent-filter>
-		</activity>
-	</application>
-</manifest>`
